10819f681da0e1826d284ce844e229b13125db51
Raunak Singh <153410858+mordorbordor@users.noreply.github.com>
2024-06-11 06:48:28 +0530

    Add files via upload
    This is the first practice session as guided by NIELIT, Calicut for Beginners in Python.

---
+++ Practice_Week_1.py
@@ -0,0 +1,9 @@
+## This practice session is inspired by NIELIT, Calicut lectures on Basics of Python. 
+# Practice_1: Write a program to include 3 favourite cricket players.
+print("My favourite cricket players are: \n MS DHONI \n SACHIN TENDULKAR \n RAHUL DRAVID")
+# Practice_2: Upside down traingle with * by using the print() function. 
+print("********* \n ******* \n  ***** \n   *** \n    *")
+# Practice_3: Escape characters by using the print() function
+print("\\\\\\\\\\Raunak \n \\\\\\\\\Singh \n  \\\\\\\\\\Doctor \n   \\\\\\\\\\Python_learner")
+# Practice _4: Print a famous quote using print() function
+print("\'Success is no accident. It is hard work, perseverance, learning, studying, sacrifice and most of all, love of what you are doing or learning to do.' \n –Pele, Brazilian Soccer Player")
