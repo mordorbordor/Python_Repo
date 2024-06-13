1fdbc55099dde646912628baeefc1e9208ba9629
Raunak Singh <153410858+mordorbordor@users.noreply.github.com>
2024-06-14 00:17:53 +0530

    Add files via upload
    This is a simple project for assessing the output age in terms of bird_years for the given human_age. 
    
    It was done as a homework for the NIELIT, Calicut course in Python.

---
+++ Age_bird_years.py
@@ -0,0 +1,4 @@
+print("Enter your age: ")
+age_human = int(input()) ## Convert the imput into an integer. The default reading is done as a strigs. 
+bird_year_age = age_human*9
+print(f"Your age in terms of bird-years is {bird_year_age}")
